2befe10c7c48f5fe259a195aea2b62a7039dffdd
abhaysheladiya <abhaysheladiya@gmail.com>
2025-07-03 18:39:06 +0530

    Contact router in backend

---
+++ server/models/contact-model.js
@@ -0,0 +1,11 @@
+const {Schema , model, connect } = require("mongoose");
+
+const contactSchema = new Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true},
+    message: { type:String, required: true},
+});
+
+
+const Contact = new model("Contact", contactSchema);
+module.exports = Contact;
