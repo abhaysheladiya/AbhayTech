572e3f9b9b2452d3489d4c19f7b23f74a200ad65
abhaysheladiya <abhaysheladiya@gmail.com>
2025-07-06 09:16:13 +0530

    Registration connected with Backend and CORS error implement.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,78 +1,136 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Register = () =>{
+export const Register = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState({
-        username: "",
-        email: "",
-        phone: "",
-        password: "",
+  //handling the input fields
+  const handleInput = (e) => {
+    console.log(e);
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
     });
-    //handling the input fields
-    const handleInput= (e) =>{
-        console.log(e);
-        let name = e.target.name;
-        let value = e.target.value;
+  };
 
-        setUser({
-            ...user,
-            [name]: value,
-        });
-    };
+  //handling form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //prevent default behaviuor of form like refresh.
+    console.log(user);
 
-    //handling form submit
-    const handleSubmit= (e) =>{
-        e.preventDefault();  //prevent default behaviuor of form like refresh.
-        console.log(user);
-    };
+    //backend connection
+    try {
+      const response = await fetch(`http://localhost:5000/api/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
 
-    return (
-        <>
-        <section>
-            <main>
-                <div className="section-registration">
-                    <div className="container grid grid-two-cols">
-                        <div className="registration-image">
-                            <img src="/images/register.png" alt="registration image" width="500" height="500"/>
-                        </div>
+      if (response.ok) {
+        setUser({ username: "", email: "", phone: "", password: "" });
+        navigate("/login");
+      }
+      console.log(response);
+      
+    } catch (error) {
+      console.log("register", error);
+    }
+  };
 
-                        <div className="registration-form">
-                            <h1 className="main-heading mb-3">Registration form</h1>
-                            <br />
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt="registration image"
+                  width="500"
+                  height="500"
+                />
+              </div>
 
-                            <form onSubmit={handleSubmit}>
-                                <div>
-                                    <label htmlFor="username">username</label>
-                                    <input type="text" name="username" placeholder="usernamr" id="username" required autoComplete="off"
-                                    value={user.username} onChange={handleInput} />
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Registration form</h1>
+                <br />
 
-                                </div>
-                                <div>
-                                    <label htmlFor="email">email</label>
-                                    <input type="email" name="email" placeholder="enter email" id="email" required autoComplete="off"
-                                    value={user.email} onChange={handleInput} />
-                                </div>
-                                <div>
-                                    <label htmlFor="phone">phone</label>
-                                    <input type="number" name="phone" placeholder="enter phone" id="phone" required autoComplete="off"
-                                    value={user.phone} onChange={handleInput} />
-                                </div>
-                                <div>
-                                    <label htmlFor="password">password</label>
-                                    <input type="password" name="password" placeholder="enter password" id="password" required autoComplete="off" 
-                                    value={user.password} onChange={handleInput}/>
-                                </div>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="usernamr"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={user.username}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="enter email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      placeholder="enter phone"
+                      id="phone"
+                      required
+                      autoComplete="off"
+                      value={user.phone}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="enter password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
 
-                                <br />
-                                <button type="submit" className="btn btn-submit">
-                                    Register Now
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </section>
-        </>
-    )
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 };
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const authRoute = require("./router/auth-router");
 const contactRoute = require("./router/contact-router");
@@ -7,6 +8,13 @@ const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 //middleware
+const corsOptions ={
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    Credential: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());   //it means now we use json
 
 app.use("/api/auth", authRoute);
