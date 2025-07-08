366259d44a1ce00d5537cb1f94878c9350348bfd
abhaysheladiya <abhaysheladiya@gmail.com>
2025-07-08 11:45:39 +0530

    Admin Authentication.

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const adminController = require("../contollers/admin-controller");
+const authMiddleware = require("../middlewares/auth-middleware");
+const adminMiddleware = require("../middlewares/admin-middleware");
+
 const router = express.Router();
 
-router.route("/users").get(adminController.getAllUsers);
-router.route("/contacts").get(adminController.getAllContacts);
+router.route("/users").get(authMiddleware, adminMiddleware, adminController.getAllUsers);
+router.route("/contacts").get(authMiddleware, adminMiddleware, adminController.getAllContacts);
 
 module.exports = router;
